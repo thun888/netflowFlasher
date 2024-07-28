d433f57e8008a2f16bd389e943284e558c4d47d0
thun888 <thun888@qq.com>
2024-07-28 20:04:41 +0800

    mod

--- netflowFlasher.go
+++ netflowFlasher.go
@@ -1,4 +1,3 @@
-// netflowFlasher 刷下行流量
 package main
 
 import (
@@ -7,30 +6,88 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
-// 下载链接
-// 星铁pc，星铁apk，崩2，崩3PC完整，原pc，原apk
-var downloadList = []string{"https://api-takumi.mihoyo.com/event/download_porter/link/hkrpg_cn/official/pc_default",
-	"https://api-takumi.mihoyo.com/event/download_porter/link/hkrpg_cn/official/android_default",
-	"https://static.benghuai.com/Download/v10_1/Original.StripResource_10.1.8_313_utw.shell.apk",
-	"https://bundle.bh3.com/ptpublic/rel/20230416151857_AsBJm4PVPKKR38YI/PC/BH3_v6.6.0_4ed7d53313df.7z",
-	"https://ys-api.mihoyo.com/event/download_porter/link/ys_cn/official/pc_default",
-	"https://ys-api.mihoyo.com/event/download_porter/link/ys_cn/official/android_default"}
+type Config struct {
+	DownloadList []string `yaml:"downloadList"`
+	Datachunk    int64    `yaml:"datachunk"`
+	Timelapse    int      `yaml:"timelapse"`
+	TrafficFile  string   `yaml:"trafficFile"`
+}
 
-// 下载限速，单位KB
-const (
-	datachunk = 3 * 1024 * 1024 // 下载限速
-	timelapse = 1               // per second
-)
+func loadConfig(filename string) (*Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func loadTraffic(filename string) (int64, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, nil
+		}
+		return 0, err
+	}
+	defer file.Close()
+
+	var totalTraffic int64
+	_, err = fmt.Fscanf(file, "%d", &totalTraffic)
+	if err != nil {
+		return 0, err
+	}
+
+	return totalTraffic, nil
+}
+
+func saveTraffic(filename string, totalTraffic int64) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "%d", totalTraffic)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
+	config, err := loadConfig("config.yaml")
+	if err != nil {
+		log.Fatalf("加载配置文件失败: %v", err)
+	}
+
+	totalTraffic, err := loadTraffic(config.TrafficFile)
+	if err != nil {
+		log.Fatalf("加载流量记录失败: %v", err)
+	}
+
+	log.Printf("上次运行总消耗流量: %d MB\n", totalTraffic)
+
 	i := 0
 	for {
 		i = i + 1
 		log.Println("第", i, "轮下载开始")
-		for n, url := range downloadList {
+		for n, url := range config.DownloadList {
 			timeSleep := time.Duration(rand.Intn(10)) * time.Second
 			log.Println("第", n, "个下载结束，等待", timeSleep)
 			time.Sleep(timeSleep)
@@ -46,8 +103,8 @@ func main() {
 
 				var alreadyDown int64
 
-				for range time.Tick(timelapse * time.Second) {
-					n, err := io.CopyN(file, resp.Body, datachunk)
+				for range time.Tick(time.Duration(config.Timelapse) * time.Second) {
+					n, err := io.CopyN(file, resp.Body, config.Datachunk)
 					if err != nil {
 						if err == io.EOF {
 							log.Println(url, "下载完成")
@@ -57,10 +114,16 @@ func main() {
 						break
 					}
 					alreadyDown = alreadyDown + n/1024/1024
+					totalTraffic += n / 1024 / 1024
 
 					log.Println("已下载" + fmt.Sprint(alreadyDown) + "兆，完成百分之" + fmt.Sprint(alreadyDown*10/contentLength*10))
 				}
 			}
 		}
+		err = saveTraffic(config.TrafficFile, totalTraffic)
+		if err != nil {
+			log.Fatalf("保存流量记录失败: %v", err)
+		}
+		log.Printf("当前总消耗流量: %d MB\n", totalTraffic)
 	}
 }
